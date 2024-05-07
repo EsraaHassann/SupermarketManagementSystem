e6bcd58ade8ad544790ec03152c2cc98b451bc40
EsraaHassann <147067443+EsraaHassann@users.noreply.github.com>
2024-05-07 20:23:29 +0300

    edits

--- SupermarketManagementSystem/CategoryForm.cs
+++ SupermarketManagementSystem/CategoryForm.cs
@@ -18,6 +18,154 @@ namespace SupermarketManagementSystem
         public CategoryForm()
         {
             InitializeComponent();
+            getTable(); // Load data into the DataGridView when the form loads
+        }
+
+        private void CategoryForm_Load(object sender, EventArgs e)
+        {
+            getTable();
+        }
+
+        private void getTable()
+        {
+            string selectQuery = "SELECT * FROM Category";
+            using (SqlCommand command = new SqlCommand(selectQuery, dBCon.GetCon()))
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dataGridView1_category.DataSource = table;
+                }
+            }
+        }
+
+        private void button1_Add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string insertQuery = "INSERT INTO Category VALUES(" + textBox1_id.Text + ", '" + textBox_Name.Text + "', '" + textBox_des.Text + "')";
+                SqlCommand command = new SqlCommand(insertQuery, dBCon.GetCon());
+                dBCon.OpenCon();
+                command.ExecuteNonQuery();
+                MessageBox.Show("Category Added Successfully", "Add Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dBCon.CloseCon();
+                getTable();
+                clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1_id.Text == "" || textBox_Name.Text == "" || textBox_des.Text == "")
+                {
+                    MessageBox.Show("Missing Information", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    string updateQuery = "UPADTE Category SET CatName = '" + textBox_Name.Text + "', CatDes = '" + textBox_des.Text + "' WHERE CatId = '" + textBox1_id.Text + "'";
+                    SqlCommand command = new SqlCommand(updateQuery, dBCon.GetCon());
+                    dBCon.OpenCon();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Category Updated Successfully", "Updated Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    dBCon.CloseCon();
+                    getTable();
+                    clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_category_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1_id.Text = dataGridView1_category.SelectedRows[0].Cells[0].Value.ToString();
+            textBox_Name.Text = dataGridView1_category.SelectedRows[0].Cells[1].Value.ToString();
+            textBox_des.Text = dataGridView1_category.SelectedRows[0].Cells[2].Value.ToString();
+        }
+
+        private void clear()
+        {
+            textBox1_id.Clear();
+            textBox_Name.Clear();
+            textBox_des.Clear();
+        }
+
+        private void button3_Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string deleteQuery = "DELETE FROM Category WHERE CatId = " + textBox1_id.Text + "";
+                SqlCommand command = new SqlCommand(deleteQuery, dBCon.GetCon());
+                dBCon.OpenCon();
+                command.ExecuteNonQuery();
+                MessageBox.Show("Category Deleted Successfully", "Delete Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dBCon.CloseCon();
+                getTable();
+                clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Red;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.CornflowerBlue;
+        }
+
+        private void button8_logout_Click(object sender, EventArgs e)
+        {
+            //LoginForm login = new LoginForm();
+            //login.Show();
+            //this.Hide();
+
+        }
+
+        private void button8_logout_MouseEnter(object sender, EventArgs e)
+        {
+            button8_logout.ForeColor = Color.Red;
+        }
+
+        private void button8_logout_MouseLeave(object sender, EventArgs e)
+        {
+            button8_logout.ForeColor = Color.CornflowerBlue;
+        }
+
+        private void button6_product_Click(object sender, EventArgs e)
+        {
+            //ProductForm product = new ProductForm();
+            //product.Show (); 
+            //this.Hide();    
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_id_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
- }
+}
