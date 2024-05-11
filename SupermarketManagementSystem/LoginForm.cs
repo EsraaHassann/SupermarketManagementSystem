2d843e61451a2965b493b169fd460ec13343312a
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2024-05-12 00:16:10 +0300

    login

---
+++ SupermarketManagementSystem/LoginForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupermarketManagementSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_clear_Click(object sender, EventArgs e)
+        {
+            TextBox_username.Clear();
+            TextBox_password.Clear();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_exit_MouseEnter(object sender, EventArgs e)
+        {
+            label_exit.ForeColor = Color.Red;
+        }
+
+        private void label_exit_MouseLeave(object sender, EventArgs e)
+        {
+            label_exit.ForeColor = Color.CornflowerBlue;
+        }
+
+        private void label_clear_MouseEnter(object sender, EventArgs e)
+        {
+            label_clear.ForeColor = Color.Red;
+        }
+
+        private void label_clear_MouseLeave(object sender, EventArgs e)
+        {
+            label_clear.ForeColor = Color.CornflowerBlue;
+        }
+
+        private void label_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
