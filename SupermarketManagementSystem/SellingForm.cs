2977b52f4c856bf84e61846c0f168b3815034360
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2024-05-13 19:37:29 +0300

    sellingform

---
+++ SupermarketManagementSystem/SellingForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupermarketManagementSystem
+{
+    public partial class SellingForm : Form
+    {
+        DBConnect dBCon = new DBConnect();
+        DGVPrinter printer = new DGVPrinter();
+        public SellingForm()
+        {
+            InitializeComponent();
+        }
+        private void getCategory()
+        {
+            string selectQuerry = "SELECT * FROM Category";
+            SqlCommand command = new SqlCommand(selectQuerry, dBCon.GetCon());
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            comboBox_category.DataSource = table;
+            comboBox_category.ValueMember = "CatName";
+        }
+        private void getTable()
+        {
+            string selectQuerry = "SELECT ProdName, ProdPrice FROM Product";
+            SqlCommand command = new SqlCommand(selectQuerry, dBCon.GetCon());
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView_product.DataSource = table;
+        }
+        private void getsellTable()
+        {
+            string selectQuerry = "SELECT * FROM Bill ";
+            SqlCommand command = new SqlCommand(selectQuerry, dBCon.GetCon());
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView_sellsList.DataSource = table;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string insertQuery = "INSERT INTO Bill VALUES(" + TextBox_id.Text + ",'" + label_seller.Text + "','" + label_date.Text + "'," + grandTotal.ToString + ")";
+                SqlCommand command = new SqlCommand(insertQuery, dBCon.GetCon());
+                dBCon.OpenCon();
+                command.ExecuteNonQuery();
+                MessageBox.Show("Order Added Successfully", "Order Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dBCon.CloseCon();
+                getsellTable();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SellingForm_Load(object sender, EventArgs e)
+        {
+            label_date.Text = DateTime.Today.ToShortDateString();
+            label_seller.Text = LoginForm.seller_Name;
+            getTable();
+            getCategory();
+            getsellTable();
+        }
+
+        private void dataGridView_product_Click(object sender, EventArgs e)
+        {
+            TextBox_name.Text = dataGridView_product.SelectedRows[0].Cells[0].Value.ToString();
+            TextBox_price.Text = dataGridView_product.SelectedRows[0].Cells[1].Value.ToString();
+        }
+        int grandTotal = 0, n = 0;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_print_Click(object sender, EventArgs e)
+        {
+            //we need DGVPrinter here for PDF FILE
+            printer.Title = "EL Gamal SuperMarket sell Lists ";//we need DGVPrinter here
+            printer.SubTitle = string.Format("Date : {0} ", DateTime.Now.Date);
+            printer.SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip;
+            printer.PageNumbers = true;
+            printer.PageNumberInHeader = false;
+            printer.PorportionalColumns = true;
+            printer.HeaderCellAlignment = StringAlignment.Near;
+            printer.Footer = "foxlearn";
+            printer.FooterSpacing = 15;
+            printer.printDocument.DefaultPageSettings.Landscape = true;
+            printer.PrintDataGridView(dataGridView_sellsList);
+        }
+
+        private void button8_logout_Click(object sender, EventArgs e)
+        {
+            LoginForm login = new LoginForm();
+            login.Show();
+            this.Hide();
+        }
+
+        private void button1_Add_Click(object sender, EventArgs e)
+        {
+            if (TextBox_name.Text == "" || TextBox_qty.Text == "")
+            {
+                MessageBox.Show("Missing Information", "Information Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                int Total = Convert.ToInt32(TextBox_price.Text) * Convert.ToInt32(TextBox_qty.Text);
+                DataGridViewRow addRow = new DataGridViewRow();
+                addRow.CreateCells(dataGridView_order);
+                addRow.Cells[0].Value = ++n;
+                addRow.Cells[1].Value = TextBox_name.Text;
+                addRow.Cells[2].Value = TextBox_price.Text;
+                addRow.Cells[3].Value = TextBox_qty.Text;
+                addRow.Cells[4].Value = Total;
+                dataGridView_order.Rows.Add(addRow);
+                grandTotal += Total;
+                label_amount.Text = grandTotal + " Ks";
+            }
+        }
+    }
+}
