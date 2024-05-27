2bbe0eb42138feb0d1c049554c06b19a88e06345
esraa <esraahassann23@gmail.com>
2024-05-27 22:02:13 +0300

    context

---
+++ SupermarketManagementSystem/ProductContext.cs
@@ -0,0 +1,24 @@
+﻿using SupermarketManagementSystem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketManagementSystem
+{
+    public class ProductContext
+    {
+        private IProductStrategy _strategy;
+
+        public void SetStrategy(IProductStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void ExecuteStrategy(Product product)
+        {
+            _strategy.Execute(product);
+        }
+    }
+}
