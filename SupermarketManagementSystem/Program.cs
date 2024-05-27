09949d3a9cb1239f6a1a3ae762d6b8fb705749e6
nadaelamir10 <nada2103057@miuegypt.edu.eg>
2024-05-27 14:29:07 +0200

    edit

--- SupermarketManagementSystem/Program.cs
+++ SupermarketManagementSystem/Program.cs
@@ -16,9 +16,9 @@ namespace SupermarketManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SellingForm());
+            //Application.Run(new SellingForm());
             //Application.Run(new SplashForm());
-          // Application.Run(new LoginForm());
+            Application.Run(new LoginForm());
             //Application.Run(new CategoryForm());
             //Application.Run(new SellerForm());
             //Application.Run(new ProductForm());
