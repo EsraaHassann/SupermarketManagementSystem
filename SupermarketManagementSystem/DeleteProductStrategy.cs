d075d21bdb54fc6aa413e356d8133a489ee53e8e
esraa <esraahassann23@gmail.com>
2024-05-27 21:51:14 +0300

    prod form

---
+++ SupermarketManagementSystem/DeleteProductStrategy.cs
@@ -0,0 +1,24 @@
+﻿using SupermarketManagementSystem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SupermarketManagementSystem
+{
+    public class DeleteProductStrategy : IProductStrategy
+    {
+        private DBConnect dBCon = new DBConnect();
+
+        public void Execute(Product product)
+        {
+            string deleteQuery = $"DELETE FROM Product WHERE ProdId = {product.Id}";
+            SqlCommand command = new SqlCommand(deleteQuery, dBCon.GetCon());
+            dBCon.OpenCon();
+            command.ExecuteNonQuery();
+            dBCon.CloseCon();
+        }
+    }
+}
